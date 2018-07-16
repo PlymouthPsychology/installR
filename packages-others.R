e1553e59e60dbecc9b2c9e6abb53130fa9e6c44b
benwhalley <ben.whalley@plymouth.ac.uk>
2018-07-16 16:21:09 +0100

    added bits for julian stander in maths/stats

--- packages-others.R
+++ packages-others.R
@@ -7,10 +7,14 @@ options("repos" = "https://mran.microsoft.com/snapshot/2018-07-01")
 
 suggested <- read_csv("package-data/packages-suggested.csv") %>% pull(Package)
 bayes <- read_csv("package-data/packages-bayes.csv") %>% pull(Package)
+mathsstats <- read_csv("package-data/packages-maths-stats.csv") %>% pull(Package)
 github <- read_csv("package-data/packages-github.csv")
 
 utils::install.packages(suggested)
+
+# dependencies=T required for rstan
 utils::install.packages(bayes, dependencies = T)
+utils::install.packages(mathsstats)
 
 github %>%
   rowwise() %>%
