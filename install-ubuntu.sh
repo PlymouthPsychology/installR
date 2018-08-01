f10530608a8ee539c14720eac7313089b613e841
benwhalley <ben.whalley@plymouth.ac.uk>
2018-08-01 13:53:41 +0100

    fixed bug by removing port number for https connection

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -33,7 +33,7 @@ sudo apt-get install -y libxml2-dev
 sudo apt-get install -y jags
 
 
-git clone https://github.com:80/PlymouthPsychology/installR
+git clone https://github.com/PlymouthPsychology/installR
 cd installR
 
 sudo R --vanilla < packages-minimal.R
