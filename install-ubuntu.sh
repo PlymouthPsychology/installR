30f8ce82a9d6938764949e282b783fa001a157f8
benwhalley <ben.whalley@plymouth.ac.uk>
2019-07-17 09:31:49 +0100

    add pandoc

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -27,6 +27,7 @@ sudo apt-get install -y libnlopt-dev # otherwise chokes when installing within R
 sudo apt-get install -y libssl-dev 
 sudo apt-get install -y libcurl4-openssl-dev 
 sudo apt-get install -y libxml2-dev
+sudo apt-get install -y pandoc
 
 # note this will install latest packaged JAGS... see windows install intructions to match version exactly
 # http://mcmc-jags.sourceforge.net
@@ -38,7 +39,6 @@ cd installR
 
 sudo R --vanilla < packages-minimal.R
 sudo R --vanilla < packages-others.R
-sudo R --vanilla < check-versions.R
 
 } # end the install function
 
