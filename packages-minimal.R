5787db8701a30ff1340b8bfcee805ac0b0ed2e5d
benwhalley <ben.whalley@plymouth.ac.uk>
2020-06-30 16:14:17 +0100

    addiitional deps for worksheets; tidying

--- packages-minimal.R
+++ packages-minimal.R
@@ -2,4 +2,8 @@
 # avoid compatibility issues.
 options("repos" = "https://mran.microsoft.com/snapshot/2020-06-29")
 minimal <- read.csv("package-data/packages-minimal.csv", stringsAsFactors = F)$Package
-utils::install.packages(minimal)
+
+ncores <- parallel::detectCores()-1
+
+update.packages()
+utils::install.packages(minimal, Ncpus=ncores, quiet=T)
--- packages-others.R
+++ packages-others.R
@@ -5,21 +5,21 @@ library(devtools)
 # avoid compatibility issues.
 options("repos" = "https://mran.microsoft.com/snapshot/2020-06-29")
 
+ncores <- parallel::detectCores()-1
+QUIET <- F
+# update.packages()
+
 suggested <- read_csv("package-data/packages-suggested.csv") %>% pull(Package)
 bayes <- read_csv("package-data/packages-bayes.csv") %>% pull(Package)
 #mathsstats <- read_csv("package-data/packages-maths-stats.csv") %>% pull(Package)
-#github <- read_csv("package-data/packages-github.csv")
 
-utils::install.packages(suggested)
+utils::install.packages(suggested, Ncpus=ncores, quiet=QUIET)
+utils::install.packages(bayes, dependencies = T, Ncpus=ncores, quiet=T) # dependencies=T required for rstan
 
-# dependencies=T required for rstan
-utils::install.packages(bayes, dependencies = T)
-utils::install.packages(mathsstats)
-
+# utils::install.packages(mathsstats, Ncpus=ncores, quiet=QUIET)
 
 # these github packages are also needed and not on cran
 # note commit ref to snapshot at specific release (more secure)
-
 devtools::install_github('benwhalley/apastats',
     ref="343cbb3f0f37df0f6f54a0df351c98d9f17d5cbc",
     subdir='apastats')
