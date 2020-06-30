f3773621b96db6c3f32b33ddf9d7e15e68dc5550
benwhalley <ben.whalley@plymouth.ac.uk>
2020-06-30 12:53:09 +0100

    update snapshot dates

--- packages-others.R
+++ packages-others.R
@@ -3,12 +3,12 @@ library(devtools)
 
 # Set the CRAN repo to a specific date to effectively snapshop and
 # avoid compatibility issues.
-options("repos" = "https://mran.microsoft.com/snapshot/2019-07-01")
+options("repos" = "https://mran.microsoft.com/snapshot/2020-06-29")
 
 suggested <- read_csv("package-data/packages-suggested.csv") %>% pull(Package)
 bayes <- read_csv("package-data/packages-bayes.csv") %>% pull(Package)
-mathsstats <- read_csv("package-data/packages-maths-stats.csv") %>% pull(Package)
-github <- read_csv("package-data/packages-github.csv")
+#mathsstats <- read_csv("package-data/packages-maths-stats.csv") %>% pull(Package)
+#github <- read_csv("package-data/packages-github.csv")
 
 utils::install.packages(suggested)
 
@@ -20,9 +20,6 @@ utils::install.packages(mathsstats)
 # these github packages are also needed and not on cran
 # note commit ref to snapshot at specific release (more secure)
 
-devtools::install_github('achetverikov/apastats', 
-    ref="bad9b3b51869d94fa870bf63209d86cbaadd2bd4", 
+devtools::install_github('benwhalley/apastats',
+    ref="343cbb3f0f37df0f6f54a0df351c98d9f17d5cbc",
     subdir='apastats')
-
-devtools::install_github('hadley/multidplyr', 
-    ref="51b772f980efe936233d97a6f0dd61543b3b7818")
